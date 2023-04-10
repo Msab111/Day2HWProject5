43609388040850a9746d32ea27ac4a8d6d7795c4
sabamansoori <sabamansoori@10.0.0.102>
2023-04-10 06:46:30 -0400

    initial commit

---
+++ src/day2hwp5/Day2hwp5Demo.java
@@ -0,0 +1,23 @@
+package day2hwp5;
+
+public class Day2hwp5Demo {
+	
+	
+public static void main (String args []) {
+		
+		System.out.println ("Day 2 hw 5 stash ");
+		
+	}
+
+
+public void add (int a, int b) {
+	
+	System.out.println (a+b);
+
+
+
+}
+
+}
+
+
